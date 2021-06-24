d5f842dc16d9c2532bfc34cdf1e9b03d44c8c2fd
Alohadancemeow <nawongsak@gmail.com>
2021-06-24 19:20:03 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,78 @@
+let searchBtn = document.querySelector('#search-btn');
+let searchBar = document.querySelector('.search-bar-container');
+let formBtn = document.querySelector('#login-btn');
+let loginForm = document.querySelector('.login-form-container');
+let formClose = document.querySelector('#form-close');
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+let videoBtn = document.querySelectorAll('.vid-btn');
+
+
+// Remove class/icoos when scroll down
+window.onscroll = () => {
+    searchBtn.classList.remove('fa-times');
+    searchBar.classList.remove('active');
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+    loginForm.classList.remove('active');
+}
+
+// Toggle to another class/icon when clicked
+menu.addEventListener('click', () => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+});
+
+searchBtn.addEventListener('click', () => {
+    searchBtn.classList.toggle('fa-times');
+    searchBar.classList.toggle('active');
+});
+
+// Add class 'active' when clicked
+formBtn.addEventListener('click', () => {
+    loginForm.classList.add('active');
+});
+
+// Remove class 'active' when clicked
+formClose.addEventListener('click', () => {
+    loginForm.classList.remove('active');
+});
+
+// Loop video buttons,
+// When each button is clicked, 
+// Remove class 'active' form prev button,
+// Then add class 'active' to current button
+// And set data-src from clicked button to video slider for displaying.
+videoBtn.forEach(btn => {
+    btn.addEventListener('click', () => {
+        document.querySelector('.controls .active').classList.remove('active');
+        btn.classList.add('active');
+        let src = btn.getAttribute('data-src');
+        document.querySelector('#video-slider').src = src;
+    });
+});
+
+
+// Set swiper
+var swiper = new Swiper(".review-slider", {
+    spaceBetween: 20,
+    loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+        1400: {
+            slidesPerView: 4,
+        }
+    },
+});
